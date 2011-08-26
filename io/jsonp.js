afa28c81ac5ff9d9d9fbee95a734b057b964b3aa
Kael <i@kael.me>
2011-08-26 22:44:43 +0800

    change comments of module jsonp

--- io/jsonp.js
+++ io/jsonp.js
@@ -1,36 +1,38 @@
-/**
- * @module  jsonp
- * @author  Kael Zhang, modified and optimized from Mootools.Request.JSON
+/*!
+ * module  jsonp
+ * author  Kael Zhang
  */
 
 
-// Kael.me JSONP Request
-;(function(K){
+// JSONP Request
+KM.define(function (K) {
 
 // @private
 // clean/check the symbol of empersand('&') on the end/beginning of a query
-    
+
 // @param query {query} the string to be tidied
 // @param hasFirst {Boolean}
-//      if true, will check whether is the has a '&' at first. if hasn't, one '&' will be added
+//      if true, will check whether there is a '&' at first. if there isn't, one '&' will be added
 //      if false, 
-function _tidyEmpersand(str, hasFirst, hasLast){
+function _tidyEmpersand(str, hasFirst, hasLast) {
     var e = '&',
-        len = str.length - 1,
-        f = str[0] === e,
-        l = str[len] === e;
-        
-    if(str){
-        if(!hasFirst){
-            str = f ? str.substr(1) : str; 
-        }else{
-            str = f ? str : e + str; 
+	    len = str.length - 1,
+	    f = str[0] === e,
+	    l = str[len] === e;
+
+    if (str) {
+        if (!hasFirst) {
+            str = f ? str.substr(1) : str;
+        } else {
+            str = f ? str : e + str;
         }
 
-        if(!hasLast){
+        len = str.length - 1;
+
+        if (!hasLast) {
             str = l ? str.substr(0, len) : err;
-        }else{
-            str = l ? str : str + e; 
+        } else {
+            str = l ? str : str + e;
         }
     }
 
@@ -39,129 +41,131 @@ function _tidyEmpersand(str, hasFirst, hasLast){
 
 // @private
 // generate a query url
-function _getQuestURL(options, query, index){
+function _getQuestURL(options, query, index) {
     var url = options.url.split('?'),
-        tidy = _tidyEmpersand;
+    	tidy = _tidyEmpersand;
 
-    return url[0] + '?' + tidy(url[1], false, true) + tidy(query, false, true) + options.callbackKey + '=KM.JSONP._request._' + index;
+    return url[0] + '?' + tidy(url[1], false, true) + tidy(query, false, true) + options.callbackKey + '=' + KM_STR + '.' + JSONP_CALLBACK_PUBLIC_PREFIX + '._' + index;
 };
 
 
-var _counter = 0, JSONP;
+var _counter = 0, JSONP,
+KM_STR = 'KM',
+JSONP_CALLBACK_PUBLIC_PREFIX = '_JSONPRequest';
 
 
 JSONP = new Class({
 
-	Implements: 'chain events options',
+    Implements: [Events, Options],
 
-	options: {
-//		onSuccess: function(data){},
-//		onCancel: function(){},
-//		onTimeout: function(){},
-		onRequest: function(src){
+    options: {
+        //		onSuccess: function(data){},
+        //		onCancel: function(){},
+        //		onTimeout: function(){},
+        onRequest: function (src) {
             K.log('JSONP retrieving script with url:' + src);
-		},
-		onError: function(src){
+        },
+        onError: function (src) {
             K.error('JSONP ' + src + ' will fail in IE, which enforces a 2083b uri length limit');
-		},
-        
+        },
+
         // @type {string} JSONP request uri
-		url: '',
+        url: '',
 
-		callbackKey: 'callback',
-//		inject: {Element} default to document.head,
-		data: '',
-		link: 'ignore',
-		timeout: 0
-	},
+        callbackKey: 'callback',
+        //		inject: {Element} default to document.head,
+        data: '',
+        timeout: 0
+    },
 
-	initialize: function(options){
+    initialize: function (options) {
         this.setOptions(options);
-	},
+    },
 
     // @param options 
     //  {Object} jsonp request data
     //  {String} jsonp request query string
 
     // @note: no more support Element type
-	send: function(options){
+    send: function (options) {
         var self = this,
-            type,
-            query,
-            src,
-            script,
-
-            // generate a unique, non-zero jsonp request id
-            index = ++ _counter,
-            k = K,
-            request = JSON._request;
-
-		if (!Request.prototype.check.call(self, options)) return self;
-		self.running = true;
+	        query,
+	        src,
+	        script,
+	
+	        // generate a unique, non-zero jsonp request id
+	        index = ++ _counter,
+	        k = K,
+	        request = k[JSONP_CALLBACK_PUBLIC_PREFIX];
+	        
+        // self.running = true;
 
         // formatting data ------------------------------------------------------- *\
-		type = k.type(options);
-		
-		// don't override new options
-        options = k.mix(type === 'string' ? {data: options} : options || {}, self.options, false);
+
+        // never override old options
         
-        query = k.toQueryString(options.data);
+        if(k.isString(options)){
+        	options = {};
+        	query = options;
+        }else{
+        	query = k.toQueryString(options.data);
+        }
+        
+        k.mix(options, self.options, false);
+        
         src = _getQuestURL(options, query, index);
 
         if (src.length > 2083) self.fireEvent('error', src);
 
         // JSONP request start
-        request['_' + index] = function(){
-            if(self.running)
+        request['_' + index] = function () {
+            if (self.running)
 
-			self.__success.apply(self, arguments);
-			
-			delete request['_' + index];
-		}
+                self.__success.apply(self, arguments);
 
-		script = self.script = _create_script(src).inject(options.inject || document.head);
+            delete request['_' + index];
+        }
 
-		self.fireEvent('request', [options.url, script]);
+        script = self.script = _create_script(src).inject(options.inject || document.head);
 
-		
-		if (options.timeout){
-			(function(){
-				if (self.running){
-					self.fireEvent('timeout', [script.get('src'), script])
-						.fireEvent('failure')
-						.cancel();
-				}
-			}).delay(options.timeout);
-		}
+        self.fireEvent('request', [options.url, script]);
 
-		return this;
-	},
+
+        if (options.timeout) {
+            (function () {
+                if (self.running) {
+                    self.fireEvent('timeout', [script.get('src'), script])
+					.fireEvent('failure')
+					.cancel();
+                }
+            }).delay(options.timeout);
+        }
+
+        return this;
+    },
 
     // @private
-	__success: function(){
-		// if (!this.running) return false;
-		this.clear()
-			.fireEvent('success', arguments)
-			.callChain();
-	},
+    __success: function () {
+        // if (!this.running) return false;
+        this.fireEvent('success', arguments);
+    },
 
-	cancel: function(){
-		return this.running ? this.clear().fireEvent('cancel') : this;
-	},
+    cancel: function () {
+        return this.running ? this.clear().fireEvent('cancel') : this;
+    },
 
-	clear: function(){
+    clear: function () {
         var self = this;
 
-		self.script && self.script.destroy();
-		self.running = false;
-		return self;
-	}
+        self.script && self.script.destroy();
+        self.running = false;
+        return self;
+    }
 
 });
 
+K[JSONP_CALLBACK_PUBLIC_PREFIX] = {};
 
-JSONP._request = {};
+return JSONP;
 
-K.JSONP = JSONP;
- 
-})(KM);
+});
