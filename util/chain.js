b5bbf3485227f070a6eea357e564a7cddd5c3e51
Kael <i@kael.me>
2011-08-27 15:59:37 +0800

    moved files

---
+++ util/chain.js
@@ -0,0 +1,95 @@
+/**
+ * author  Kael Zhang
+ * diff with mootools', acting as a plugin and unobtrusively making a method chain supported
+ */
+
+KM.define(function(K){
+
+
+/**
+ * method to set current method to trigger the pipline chain
+ 
+ * @param methodName {String} method name of the current instance
+ * @param judge {Function}
+ * if returns
+ *		- 'chain'	: chain the method
+ *		- 'stop'	: stop the method
+ *		-  other	: exec the method
+ */
+function setChain(methodName, judge, host){
+	var method = host[methodName];
+	
+	if(method){
+		host[methodName] = function(){
+			var self = this,
+				result = judge && judge.call(self);
+			
+			switch(result){
+				case 'chain':
+					self.chain(function(){
+						method.apply(self, arguments);
+					});
+					break;
+						
+				case 'cancel':
+					break;
+				
+			}
+			
+			return result || method.apply(self, arguments);
+		}
+	}
+	
+	return host;
+};
+
+
+var Chain = new Class({
+
+	_chain: [],
+
+	chain: function(){
+		this._chain.append(Array.flatten(arguments));
+		return this;
+	},
+
+	callChain: function(){
+		return (this._chain.length) ? this._chain.shift().apply(this, arguments) : false;
+	},
+
+	clearChain: function(){
+		this._chain.length = 0;
+		return this;
+	},
+
+	setChain: function(methodName, judge){
+		return setChain(methodName, judge, this);
+	}
+});
+
+/**
+ * unobtrusively make a object or a class chain supported
+ * @param {string} methodName
+ * @param {function()} judge
+ * @param {function()|Object} host class or class instance or plain object
+ */
+Chain.setChain = function(methodName, judge, host){
+	if(K.isFunction(host)){
+		host.implement(Chain);
+		host = host.prototype;
+	}
+	
+	K.isObject(host) && setChain(methodName, judge, host);
+};
+
+return Chain;
+
+});
+
+
+/**
+ TODO:
+ 
+ A. merge Chain and ASQueue together as Queue
+ 
+ */
