17b1aeed9abca50fcb343b1079a776b17798e388
Kael <i@kael.me>
2011-09-09 00:06:55 +0800

    dom/clean: renaming the module name as dom, making it a fake package module

--- src/neuron/web/dom/clean.js
+++ src/neuron/web/dom/clean.js
@@ -2,25 +2,35 @@
  * clean unexpected public members
  * after this, they could only be fetched by Neuron Loader
  */
+
 ;(function(K){
 
-var DOM = K.DOM,
-	SELECTOR = K.__SELECTOR;
+var DOM = K.DOM;
+
+// store selector on DOM for further improvement
+DOM.SELECTOR = K.__SELECTOR;
+
 
 // remove public members
 delete K.DOM;
 delete K.__SELECTOR;
-
-
-DOM.SELECTOR = SELECTOR;
+delete DOM.methods;
+delete DOM.feature;
 
 
 K.define.on();
 
-K.define('DOM', function(){
-	return DOM;
-});
-
+// fake package module
+K.define('dom', function(){ return DOM; });
 K.define.off();
 
 })(KM);
+
+
+/**
+ change log:
+ 
+ 2011-09-08  Kael:
+ - renaming the module name as dom, making it a fake package module
+ 
+ */
