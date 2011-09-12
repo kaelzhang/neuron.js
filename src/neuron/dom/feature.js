7672e63d0c45fd4d34ffc128ebdc264ad4d83010
Kael <i@kael.me>
2011-09-12 17:35:14 +0800

    dom/feature: add support to getComputedStyles, event handlers

--- src/neuron/dom/feature.js
+++ src/neuron/dom/feature.js
@@ -10,7 +10,11 @@ KM.DOM.feature = function(){
 	};
 
 	var DOC = document,
+		defaultView = DOC.defaultView,
 		element_test = DOC.createElement('div'),
+		ADD_EVENT_LISTENER = 'addEventListener',
+		REMOVE_EVENT_LISTENER = 'removeEventListener',
+		
 		a;
 	
 	element_test.innerHTML = ' <link/><table></table><a href="/a" style="top:1px;float:left;opacity:.7;">a</a><input type="checkbox"/>';
@@ -19,13 +23,27 @@ KM.DOM.feature = function(){
 	
 	return {
 		
+		computedStyle: !!(defaultView && defaultView.getComputedStyle),
+		
 		// in some old webkit(including chrome), a.style.opacity === '0,7', so use regexp instead
 		// ref: 
-		opacity		: /^0.7$/.test(a.style.opacity),
+		opacity: /^0.7$/.test(a.style.opacity),
 		
-		compactEl	: !DOC.compatMode || DOC.compatMode === 'CSS1Compat' ?
+		compactEl: !DOC.compatMode || DOC.compatMode === 'CSS1Compat' ?
 			  function(doc){ return doc.documentElement; } 
-			: function(doc){ return doc.body; }
+			: function(doc){ return doc.body; },
+			
+		addEvent: element_test[ADD_EVENT_LISTENER] ?
+			  function(el, type, fn){ el[ADD_EVENT_LISTENER](type, fn, false); }
+			: function(el, type, fn){ el.attachEvent('on' + type, fn); },
+		
+		removeEvent: element_test[REMOVE_EVENT_LISTENER] ?
+			  function(el, type, fn){ el[REMOVE_EVENT_LISTENER](type, fn, false); }
+			: function(el, type, fn){ el.detachEvent('on' + type, fn); }
+			
 	};
 
 }();
+
+
+
