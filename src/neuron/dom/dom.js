442f88fe59a3b300975bdbfc95465f574c805ad5
Kael <i@kael.me>
2011-09-15 16:56:08 +0800

    dom: fix a bug about dom creation

--- src/neuron/dom/dom.js
+++ src/neuron/dom/dom.js
@@ -6,6 +6,23 @@
 ;(function(K, NULL){
 
 
+/**
+ nodeType: 
+	Node.ELEMENT_NODE == 1
+	Node.ATTRIBUTE_NODE == 2
+	Node.TEXT_NODE == 3
+	Node.CDATA_SECTION_NODE == 4
+	Node.ENTITY_REFERENCE_NODE == 5
+	Node.ENTITY_NODE == 6
+	Node.PROCESSING_INSTRUCTION_NODE == 7
+	Node.COMMENT_NODE == 8
+	Node.DOCUMENT_NODE == 9
+	Node.DOCUMENT_TYPE_NODE == 10
+	Node.DOCUMENT_FRAGMENT_NODE == 11
+	Node.NOTATION_NODE == 12
+ */
+
+
 // create element
 function DOM(element, attributes){
 	var self = this;
@@ -130,7 +147,7 @@ DOM.create = function(fragment, attributes){
 	var element;
 	
 	if(typeof fragment === 'string'){
-		element = DOC.createElement(selector);
+		element = DOC.createElement(fragment);
 	}
 	
 	return element;
