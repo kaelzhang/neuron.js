80af58ce31a1ba9605ea09bdfe4aebafdd2b6762
Kael <i@kael.me>
2011-10-11 23:28:58 +0800

    cleaner: adjust

--- src/neuron/cleaner.js
+++ src/neuron/cleaner.js
@@ -1,9 +1,11 @@
 /**
- * clean unexpected public members
+ * clean unexpected public members, making them private.
  * after this, they could only be fetched by Neuron Loader
  */
 
 ;(function(K){
+
+var DOM = K.DOM;
 
 // remove public members
 delete K.DOM;
@@ -11,6 +13,19 @@ delete K.__SELECTOR;
 delete K.__;
 delete DOM.methods;
 delete DOM.feature;
+
+
+// remove Slick from window
+// Slick is defined with 'this.Slick'
+// so, it's removable and is not [DontDelete]
+
+// IE6 - IE8 don't support delete a property of window, even if it's defined with this.MyNameSpace 
+try{
+	delete window.Slick;
+}catch(e){
+	K.log('del Slick err');
+}
+
 
 })(KM);
 
