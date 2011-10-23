bdd2eb994565f64321874da07b201e1f3149dbfe
Kael <i@kael.me>
2011-10-23 23:16:33 +0800

    unit-test: update tpl.php to add reset.css

--- unit-test/tpl.php
+++ unit-test/tpl.php
@@ -2,10 +2,16 @@
 
 function inc_unit($name, $integration = false, $use_simple = false){
 
+	$root = __DIR__;
+
 	if(!$integration){
 
-?><!DOCTYPE html><html><head><meta charset="UTF-8">
+?><!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
 <title>Unit-Test:<?php echo $name ?>.js</title>
+<link rel="stylesheet" href="/unit-test/reset.css" type="text/css" />
 <style>
 .neuron{width:700px; margin:0 auto; padding-top:10px;}.neuron .cate{display:block; font-family: Verdana; font-size: 20px; padding-top:15px; color:#666; text-transform: uppercase; margin-bottom:10px;}.neuron p{padding:0 0 10px; margin:0;}.neuron a, .neuron .title{line-height:20px; font-size: 18px; padding-bottom:1px; margin-left:20px; font-family: Verdana; color:#1376b6; text-decoration: none; border-bottom:1px solid #fff;}.neuron a:hover{border-bottom:1px solid #80c0ea;}.neuron a span, .neuron .title span{color:#80c0ea; font-size:12px;}
 </style>
@@ -23,7 +29,6 @@ var r = + new Date,
 
 	}
 
-	$root = __DIR__;
 	preg_match('/[-a-z\d]+$/i', $name, $brief_name);
 	
 	if($brief_name){
