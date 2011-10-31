6f1e24642e1d1bda923819bfbb8194b90ce90554
Kael <i@kael.me>
2011-10-31 21:11:40 +0800

    loader: add errcode map

---
+++ src/neuron/loader/errcode.js
@@ -0,0 +1,12 @@
+ERROR_CODE = {
+	'100' : 'def a path > {0} < may cause further problems',	// name
+	'110' : 'module {0} has not explicitly defined!', // id
+	'120' : 'cyclic dependency detected!',
+	'130' : 'define a user module with namespace is forbidden: {0}', // id
+	
+	'500' : 'Unexpected factory type for > {0} < : {1}', // name, type
+				
+	'510' : 'mod with no factory detected in a module file',
+	'530' : 'Invalid module path: {0}', // path
+	'540' : 'namespace is not defined: {0}. make sure the configuration is exactly correct', // namespace
+}
