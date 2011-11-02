44918615476601eab210921734523ca39430b3ca
Kael <i@kael.me>
2011-11-02 17:36:16 +0800

    Switch plugin/step: complete
    - complete plugin::step
    - refractor _getItem method, so it authentically created new item and
    apply to precise position

--- src/switch/step.js
+++ src/switch/step.js
@@ -5,10 +5,32 @@ var
 ITEM_RENDERER = 'itemRenderer',
 
 METHODS_OVERRIDEN = {
-	_getItem: function(index){
-		var self = this;
+
+	/**
+	 * override the _getItem method
+	 * so it could generate unexisted items
+	 * @param {number} index
+	 * @param {boolean=} dontSetPos if true, _getItem method will not set the position of newly created item
+	 */
+	_getItem: function(index, dontSetPos){
+		var self = this,
+			item = self.items[index];
+			
+		if(!item){
+			item = 
+			self.items[index] = self[ITEM_RENDERER].call(self, index).inject(self.container);
+			
+			dontSetPos || item.css(self.get('direction'), self._getOffset(index) * self.get('itemSpace'));
+		}
 		
-		return self.items[index] || (self.items[index] = self[ITEM_RENDERER].call(self, index));
+		return item;
+	},
+	
+	/**
+	 * 
+	 */
+	_getOffset: function(index){
+		return index;
 	}
 };
 
@@ -16,25 +38,43 @@ METHODS_OVERRIDEN = {
 return {
 	name: 'step',
 	
-	options: {
+	ATTRS: {
 		// async: false,
 		
 		/**
-		 * @type {function(index, callback)}
+		 * method to render the Neuron/DOM instance of the item of expected index
 		 * @returns {string} the Neuron/DOM instance of the item with <index> index
 		 */
-		itemRenderer: null
+		itemRenderer: {
+		
+			// type {function(index, callback)}
+			// validator: K.isFunction
+		},
+		
+		dataLength: {
+			value: K.isNumber,
+			getter: function(v){
+				return v || 0;
+			}
+		},
+		
+		itemSpace: null
 	},
 	
 	init: function(self){
 		var EVENTS = self.get('EVENTS');
+		
+		self.on(EVENTS.BEFORE_INIT, function(){
+			K.mix(this, METHODS_OVERRIDEN);
+		});
 	
 		self.on(EVENTS.AFTER_INIT, function(){
-			var t = this;
+			var self = this;
 			
-			K.mix(t, METHODS_OVERRIDEN);
+			self[ITEM_RENDERER] = self.get(ITEM_RENDERER);
 			
-			t[ITEM_RENDERER] = t.get(ITEM_RENDERER);
+			// set fake length value
+			self._itemData(self.length + self.get('dataLength')); console.log(self.get('dataLength'))
 		});
 	}
 };
