42d7730e4db644aecfe74f079d7af21ee1bba583
Kael <i@kael.me>
2011-11-18 11:15:25 +0800

    dom/css: save currentCSS method to KM.DOM.feature

--- src/neuron/dom/css.js
+++ src/neuron/dom/css.js
@@ -156,7 +156,7 @@ var DOM = K.DOM,
 
 // @private
 // get computed styles
-currentCSS = feature.computedStyle ? 
+currentCSS = feature.curCSS = feature.computedStyle ? 
 
 	// standard
 	function(element, property){
