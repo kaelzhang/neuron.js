ca7515190f63d46f1388d6bbab220bf882c043d1
Kael <i@kael.me>
2012-09-03 11:20:25 +0800

    test/unit: add test cases for neuron seed

---
+++ test/unit/neuron/seed.js
@@ -0,0 +1,11 @@
+describe("Neuron: seed", function(){
+    
+describe("NR._debugOn()", function(){
+    it("will switch on debug mode", function(){
+        NR._debugOn();
+        
+        expect(NR._env.debug).toBe(true);
+    });
+});
+    
+});
