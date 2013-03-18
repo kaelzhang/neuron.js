50ed8b6878aa1c8ac8c44c9058c896bdd0fa305c
Kael <i@kael.me>
2013-03-18 18:02:56 +0800

    src/app: add nc/demos.js

---
+++ src/app/nc/demos.js
@@ -0,0 +1,37 @@
+var
+
+Menu = require('~/mod/menu'),
+Ajax = require('io/ajax');
+    
+
+module.exports = {
+    init: function(config) {
+        NR.ready(function(){
+            var content = $('#content');
+
+            new Menu({
+                tree: config.demos.children,
+
+                parser: function(t) {
+                    t.forEach(function(li) {
+                        if(li.children){
+                            delete li.path;
+                        }
+                    });
+
+                    return t;
+                }
+
+            }).on({
+                menuClick: function(e) {
+                    var link = e.link;
+
+                    content.empty();
+
+                    $.create('iframe', {src: link}).inject(content);
+
+                }
+            });
+        });
+    }
+};
