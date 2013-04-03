3b3b93adbd0eb27a94f90120518d973fd4645709
yuanxiang.yang <yuanxiang.yang@dianping.com>
2013-04-03 11:07:30 +0800

    upgrade/lib/filtration:Processing components, and business components

--- upgrade/lib/filtration/app.js
+++ upgrade/lib/filtration/app.js
@@ -1,74 +1,138 @@
-var 
-	http = require("http"),
-	fs = require('fs'),
-	url = require('url'),
-	querystring = require('querystring');
+	//加载模块
+	
+	var http = require("http"),
+			fs = require('fs'),
+			mod_url = require('url'),
+			querystring = require('querystring');
+	
+	//加载文件
+	
+	var UglifyJS = require('uglify-js'),
+			upgrade = require('../converter'),
+			analyzer = require('../util/analyzer'),
+			wrapdefine = require('./wrapdefine');
+	
+	//  全局常量
+	
+	var APP_BASE = 's/j/app/';
+	var LIB_BASE = 'lib/1.0/';
+	
+	//  全局变量
 
-var 
-	UglifyJS = require('uglify-js'),
-	upgrade = require('../converter'),
-	analyzer = require('../util/analyzer'),
-	wrapdefine = require('./wrapdefine');
+	var server = http.createServer(function(req,response){
+	//url path对象
+	var queryObject = querystring.parse(mod_url.parse(req.url).query),
+			// 	file_content = require(file['url'] || "");
+	// 文件url
+			file =  queryObject.url || "";
+	
+	
 
-var server = http.createServer(function(req,response){
-	var queryObject = querystring.parse(url.parse(req.url).query),
-
-		// 	file_content = require(file['url'] || "");
-		urlReg = /((?:http|https):\/\/[^\/]+)(.+)/ig,
-
-		file =  queryObject.url || "",
-
-		urlA;
-
-	if(!file){
-		console.log("请求文件地址不存在")
-		return ;
-	}
-
-	urlA = urlReg.exec(file);
-
-
+	//url相关参数对象		
+	var url_object = mod_url.parse(file);
+  //文件类型匹配判断
+  var index;
+  // lib 目录
+  var libDir;
+  //文件相对地址
+  var fileId;
+	//返回js内容
 	var content = "";
 
-	http.get(urlA[1] + urlA[2], function(res) {
+			if(!file){
+				console.log("请求文件地址不存在")
+				return ;
+			}
 
-		res.setEncoding('utf8');
-		
-		res.on('data',function(chunk){
-					 
-			content+=chunk;
+  
 
-		});
+	    if( (index = url_object.pathname.indexOf(LIB_BASE)) !== -1 ){
+	       
+	        fileId = url_object.pathname.slice( index + LIB_BASE.length ).replace(/\.js$/, '');
+	   			libDir ="../../../lib/"+fileId;
+	        if(fs.existsSync(libDir)){
+	        	
+	        	var files =  fs.readdirSync(libDir),
+
+	        			index = files.length-1;
+
+	        			for(;index+1;index--){
+	        				var _file = files[index];
+	        				if(_file.indexOf(".js")!== -1){
+	        					content += fs.readFileSync(libDir+"/"+_file).toString();
+	        				}
+	        			}
+	        			
+	        			ast = upgrade.parse(content);
+
+				  		  ast = upgrade.convert(ast);
+
+				  	    analyzer(ast);
+
+				  		  code = upgrade.printCode(ast);
+
+				  		 response.writeHead(200);
+				  		 
+				  		 response.write(wrapdefine(code,fileId));
+
+				  		 response.end();
+				  		 
+	        }else{
+	        	console.log("组件不存在");
+	        }
+
+
+	    
+	    }else if( (index = url_object.pathname.indexOf(APP_BASE)) !== -1){
+	        
+	        fileId = url_object.pathname.slice( index + APP_BASE.length ).replace(/\.js$/, '').replace('/', '::');
+	    		
+					
+
+					http.get(file, function(res) {
+
+						res.setEncoding('utf8');
+						
+						res.on('data',function(chunk){
+									 
+							content+=chunk;
+
+						});
 
 
 
-		res.on('end',function(){
-	
-			     ast = upgrade.parse(content);
+						res.on('end',function(){
+					
+							     ast = upgrade.parse(content);
 
-		  		 ast = upgrade.convert(ast);
+						  		 ast = upgrade.convert(ast);
 
-		  	   analyzer(ast);
+						  	   analyzer(ast);
 
-		  		 code = upgrade.printCode(ast);
+						  		 code = upgrade.printCode(ast);
 
-		  		 response.writeHead(200);
-		  		 
-		  		 response.write(wrapdefine(code, req.url));
+						  		 response.writeHead(200);
+						  		 
+						  		 response.write(wrapdefine(code,fileId));
 
-		  		 response.end();
+						  		 response.end();
 
-		});
+						});
 
-		res.on('error',function(chunk){
+						res.on('error',function(chunk){
 
-				console.log("文件加载失败");
+								console.log("文件加载失败");
 
-		});
+						});
 
 
-	}).end();
+					}).end();
 
+
+	    }else{
+
+
+	    }
 
  }).listen(2000);
 
--- upgrade/lib/filtration/wrapdefine.js
+++ upgrade/lib/filtration/wrapdefine.js
@@ -3,14 +3,12 @@ var mod_url = require('url');
 var globalNameSpace = "NR";
 var REGEX_EXECUTOR_REQUIRE = /[^.]\brequire\((["'])([a-zA-Z0-9-\/.~:]+)\1\)/g;
 
-var APP_BASE = 's/j/app/';
-var LIB_BASE = 'lib/1.0/';
 
 
-function standardize(origin){
-    var ast = jsp.parse(origin.toString());
-    return pro.gen_code(ast);
-};
+// function standardize(origin){
+//     var ast = jsp.parse(origin.toString());
+//     return pro.gen_code(ast);
+// };
 
 function parseRequires(code){
     var m,
@@ -30,27 +28,11 @@ function parseRequires(code){
 // appbase -> app
 // 
 // 
-module.exports = function(origin, url){
+module.exports = function(origin,id){
     var code = origin;
     var reqs = parseRequires(origin).map(function(req){return "\"" + req + "\"";});
-    var query_url = mod_url.parse(url, true).query.url;
 
-    if(!query_url){
-        console.log('query url not defined');
-        process.exit(1);
-    }
-
-    var url_object = mod_url.parse(query_url);
-
-    var index;
-    var id;
-
-    if( (index = url_object.pathname.indexOf(LIB_BASE)) !== -1 ){
-        id = url_object.pathname.slice( index + LIB_BASE.length ).replace(/\.js$/, '');
     
-    }else if( (index = url_object.pathname.indexOf(APP_BASE)) !== -1){
-        id = url_object.pathname.slice( index + APP_BASE.length ).replace(/\.js$/, '').replace('/', '::');
-    }
 
     if(reqs.length >0){
         return "NR.define(\"" + id + "\", ["+reqs.join(",")+"], function(require, exports, module){var $ = NR.DOM;\n"+origin+"\n});";
