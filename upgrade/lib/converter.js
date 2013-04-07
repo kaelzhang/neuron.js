43bcc323ae54c59224a1f6470444b30660a58bd0
Kael <i@kael.me>
2013-04-07 16:20:49 +0800

    transformers: rename .js -> dom.js

--- upgrade/lib/converter.js
+++ upgrade/lib/converter.js
@@ -14,7 +14,7 @@ UglifyJS = require('uglify-js'),
 TRANSFORMER_ROOT = './transformer/',
 
 
-PRESET_TRANSFORMERS = [ 'nr', 'exports' ,'$','$name', 'remove-wrapper'];
+PRESET_TRANSFORMERS = [ 'nr', 'exports' ,'$','dom', 'remove-wrapper'];
 
 
 
