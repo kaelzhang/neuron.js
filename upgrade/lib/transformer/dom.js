9d4a23cdfcd2299670fc56e1bec9a03dad32bf88
yuanxiang.yang <yuanxiang.yang@dianping.com>
2013-04-08 18:06:03 +0800

    upgrade/lib:add log

--- upgrade/lib/transformer/dom.js
+++ upgrade/lib/transformer/dom.js
@@ -6,13 +6,31 @@
 var
 
 UglifyJS = require('uglify-js'),
-map = {
+
+log =  require('../neuron/log'),
+neuronDom =  require('../neuron/neuron-dom'),
+
+map_1 = {
   get : "eq",
   match : "is",
   el : "get",
+  all : "find",
+  count : "length",
   one : "find",
-  all : "find"
-};
+  forEach : "forEach"
+},
+
+map_2 = {
+  prev:'prev',
+  prevAll:'prevAll',
+  next:'next',
+  nextAll:'nextAll',
+  children:'children',
+  parent:'parent',
+  parents:'parents'
+},
+
+
 
 handler = {
     before: function(node){
@@ -21,104 +39,96 @@ handler = {
         if(node.CTOR === UglifyJS.AST_Call){
             
             var dotAST = node.expression || "";
-                
-                //判断为$("")类型的
-                if(dotAST && dotAST.CTOR === UglifyJS.AST_Dot && dotAST.expression && dotAST.expression.CTOR === UglifyJS.AST_Call
-                   &&  dotAST.expression.expression && dotAST.expression.expression.CTOR ===  UglifyJS.AST_SymbolRef && dotAST.expression.expression.name === "$"){
-                   
-                    var  _property = dotAST.property || "";
-                   
-                      switch(_property){
+            var _property = dotAST.property || "";
+            
+            //判断为$("")类型的
+            if(dotAST && (map_1[_property] || map_2[_property])){
+                 if(!neuronDom.is(dotAST)){
+                      log(dotAST);
+                  }
+                // $.forEach = > 不变
+                if(_property === "forEach"){
+                    return node;
+                }else{
+                    var _expression;
+                    //$.count() = >$.length
+                    if(_property === "count"){
 
-                        // $.forEach = > 不变
-                          case "forEach":
-                              return node;
+                        return new UglifyJS.AST_Dot({
+                                      expression : dotAST.expression,
+                                      property : "length"
+                                });
+                    }
+                    
+                    if(map_2[_property] || _property === "one" || _property === "all"){
+                         _expression = new UglifyJS.AST_Call({
 
+                            expression: new UglifyJS.AST_Dot({
+                                            expression : dotAST.expression,
+                                            property : "eq"
+                            }),
 
-
-                          default:
-                              var _expression;
-                          //$.count() = >$.length
-                              if(_property === "count"){
-
-                                  return new UglifyJS.AST_Dot({
-                                                      expression : dotAST.expression,
-                                                      property : "length"
-                                          });
-                              }
-
-                              
-                              if(_property === "one" || _property === "all" || !map[_property]){
-                                   _expression = new UglifyJS.AST_Call({
-
-                                      expression: new UglifyJS.AST_Dot({
-                                                      expression : dotAST.expression,
-                                                      property : "eq"
-                                                   }),
-
-                                      args : [
-                                              new UglifyJS.AST_Number({
-                                                    value : 0                                
-                                              })
-                                      ]                                                                                                      
-                                    });
-                              }else{
-                                  _expression = dotAST.expression;
-                              }
+                            args : [
+                                    new UglifyJS.AST_Number({
+                                          value : 0                                
+                                    })
+                            ]                                                                                                      
+                          });
+                    }else{
+                        _expression = dotAST.expression;
+                    }
 
 
-                              if(map[_property]){ 
-                                  _expression = new UglifyJS.AST_Call({
+                    if(map_1[_property]){ 
+                        _expression = new UglifyJS.AST_Call({
 
-                                                        expression : new UglifyJS.AST_Dot({
-                                                            expression : _expression,
-                                                            property : map[_property]
+                                              expression : new UglifyJS.AST_Dot({
+                                                  expression : _expression,
+                                                  property : map_1[_property]
 
-                                                         }),
-                                                        args : node.args
-                                                })
-                              }
+                                               }),
+                                              args : node.args
+                                      })
+                    }
 
-                              if(_property === "one"){
-                                  _expression = new UglifyJS.AST_Call({
+                    if(_property === "one"){
+                        _expression = new UglifyJS.AST_Call({
 
-                                      expression: new UglifyJS.AST_Dot({
-                                                      expression : _expression,
-                                                      property : "eq"
-                                                   }),
+                            expression: new UglifyJS.AST_Dot({
+                                            expression : _expression,
+                                            property : "eq"
+                                         }),
 
-                                      args : [
-                                              new UglifyJS.AST_Number({
-                                                    value : 0                                
-                                              })
-                                      ]                                                                                                      
-                                    });
-                              }
+                            args : [
+                                    new UglifyJS.AST_Number({
+                                          value : 0                                
+                                    })
+                            ]                                                                                                      
+                          });
+                    }
 
-                              return _expression;
-                                                   
-                      }
-
-
+                    return _expression;
+                                             
+                }
 
 
-                 }
-          }
-
+            }
+        }
+    },
             //var expression = node.expression;
 
 
 
             //处理$("") = > $("").eq(0)
-       			
+            
 
-        		// if(expression && expression.CTOR === UglifyJS.AST_SymbolRef && expression.name === "$"){
+            // if(expression && expression.CTOR === UglifyJS.AST_SymbolRef && expression.name === "$"){
 
-        				// return  new UglifyJS.AST_Call({
+                // return  new UglifyJS.AST_Call({
 
             //                 expression: new UglifyJS.AST_Dot({
-            //                              		expression : node,
-            //                              		property : "eq"
+            //                                  expression : node,
+            //                                  property : "eq"
             //                              }),
 
             //                 args : [
@@ -127,13 +137,21 @@ handler = {
             //                         })
             //                 ]                                                                                                      
             //             });
-        	 //}
+           //}
 
         
-    },
+  
 
     after: function(node){
        
+    },
+
+    setup: function(){
+
+    },
+
+    tearDown: function(){
+        neuronDom.clear_cache();
     }
 };
 
