4e9feb40f885ec71ad730c6db4b968af48bfa0d5
yuanxiang.yang <yuanxiang.yang@dianping.com>
2013-04-09 15:21:41 +0800

    lib/transformer/class.js:add class transformer

---
+++ upgrade/lib/transformer/class.js
@@ -0,0 +1,32 @@
+//////////////////////////////////////////////////////////////////
+// manage global $ definitions and references
+//////////////////////////////////////////////////////////////////
+
+
+var
+
+UglifyJS = require('uglify-js'),
+
+handler = {
+    
+    before: function(node,descend){
+         if(node.CTOR === UglifyJS.AST_New && node.expression.CTOR === UglifyJS.AST_SymbolRef && node.expression.name === "Class"){
+            return new UglifyJS.AST_New({
+                expression: new UglifyJS.AST_Dot({
+                    expression : new UglifyJS.AST_SymbolRef({
+                        name : "NR"
+                    }),
+                    property: "Class"
+                }),
+                args:node.args
+            });
+      }
+    },
+
+    after: function(node){
+       
+    }
+};
+
+
+module.exports = handler;
