4cb43122e67e67c4aeb9b3e9c123a614058c4fc9
kaelzhang <i@kael.me>
2013-12-19 16:48:22 +0800

    fixes #49: config: fix config.loaded

--- dist/neuron.js
+++ dist/neuron.js
@@ -14,7 +14,7 @@
 
 'use strict';
 
-// version 3.5.3
+// version 3.5.4
 // build 2013-12-19
 
 // including sequence: see ../build.json
@@ -1243,22 +1243,7 @@ var CONF_ATTRIBUTES = {
 
     loaded: {
         S: function (loaded) {
-            neuron_loaded.push.apply(
-                neuron_loaded, 
-                splitIfNotArray(loaded).map(function (pkg) {
-                    var splitted = pkg.split(STR_VERSION_SPLITTER);
-                    var name = splitted[0];
-                    var version = parseSemver(splitted[1]);
-
-                    if ( version ) {
-                        return name + STR_VERSION_SPLITTER + version.range;
-                    } else {
-                        version = name + STR_VERSION_SPLITTER + 'latest';
-                        neuron_loaded.push(version);
-                    }
-
-                }).filter(Boolean)
-            );
+            neuron_loaded = neuron_loaded.concat(loaded);
         },
 
         C: function (){
