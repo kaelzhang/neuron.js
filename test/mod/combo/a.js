8b48e491b49fb4521b552bbcd68c411b8ab1a838
kaelzhang <i@kael.me>
2014-03-09 13:34:04 +0800

    fixes #58: add test cases

--- test/mod/combo/a.js
+++ test/mod/combo/a.js
@@ -1,14 +1,13 @@
-define('combo-a@0.0.0/a', [], function (require, exports, module) {
-    exports.a = function(n){
-        return n;
-    } 
-});
-
-
 define('combo-a@0.0.0', ['./a'], function (require, exports, module) {
     var a = require('./a');
 
     exports.init = function (n) {
         return a.a(n);
     }
+});
+
+define('combo-a@0.0.0/a', [], function (require, exports, module) {
+    exports.a = function(n){
+        return n;
+    } 
 });
