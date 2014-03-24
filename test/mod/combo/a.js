2b9456f1bee3057859551fd4bfe43eb2bba3a2fe
kaelzhang <i@kael.me>
2014-03-25 00:52:29 +0800

    #66: update combo test case

--- test/mod/combo/a.js
+++ test/mod/combo/a.js
@@ -4,6 +4,8 @@ define('combo-a@0.0.0', ['./a'], function (require, exports, module) {
     exports.init = function (n) {
         return a.a(n);
     }
+}, {
+    main: true
 });
 
 define('combo-a@0.0.0/a', [], function (require, exports, module) {
