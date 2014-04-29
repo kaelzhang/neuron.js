d71ccc17435c9a1064f7fe3b9120ef2520e7ee48
kaelzhang <i@kael.me>
2014-04-30 01:45:57 +0800

    #83: adds comments to explain why we should use lazy initialization

--- test/mod/lazy/latest/lazy.js
+++ test/mod/lazy/latest/lazy.js
@@ -3,6 +3,8 @@ define('lazy@latest', ['./booooooom', './ok'], function(require, exports, module
   if (true) {
     module.exports = require('./ok');
   } else {
+
+    // If we invoke the factory of boooooom before `require()`d, it will booooooom !
     module.exports = require('./booooooom');
   }
 
@@ -14,6 +16,7 @@ define('lazy@latest', ['./booooooom', './ok'], function(require, exports, module
 define('lazy@latest/booooooom', [], function(require, exports, module){
   throw new Error('Boooooooom! The sky will fall upon over your head.');
 });
+
 
 define('lazy@latest/ok', [], function(require, exports, module){
   module.exports = {
