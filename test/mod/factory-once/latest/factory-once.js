224b3815ff27676928e6b67a8043265bb49a3f41
kaelzhang <i@kael.me>
2014-04-30 01:41:30 +0800

    #83: add comments about how to BOOOOOOOOOOM!

--- test/mod/factory-once/latest/factory-once.js
+++ test/mod/factory-once/latest/factory-once.js
@@ -22,3 +22,34 @@ define('factory-once@latest/index', ['./booooooom'], function(require, exports, 
 }, {
   main: true
 });
+
+
+// NON-lazy-initialization:
+// 1. define dep, 
+// 2. register boom (dependency)
+// 3. define boom
+// 4. no dependency -> run boom factory start >>> (not lazy)
+// 5. boom.exports
+// 6. async dep -> register dep
+
+// <<< boom factory end
+
+// 7. emit boom ready
+// 8. run dep factory
+// 9. require(boom) by dep -> boom.exports
+// 10. require(boom) by main -> boom.exports
+
+
+// Lazy-initialization
+// 
+// 1. define dep
+// 2. register boom
+// 3. define boom -> DO NOT run boom factory (lazy)
+// 4. emit boom ready
+// 5. emit dep ready
+// 6. require(boom) by main ->
+// 7. run boom factory start >>> (lazy)
+// 8. async dep
+// 9. run dep factory start >>>
+// 10. require(boom) -> run boom factory start >>> BOOOOOOOOOOOM !
+
