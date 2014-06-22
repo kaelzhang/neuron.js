28797d9d4664b813d20c451f1f499817da64288a
kaelzhang <i@kael.me>
2014-06-22 11:49:04 +0800

    test: fixes tab indents and cases; #103

--- test/spec/facade.js
+++ test/spec/facade.js
@@ -6,96 +6,96 @@ var module_require_2_inited;
 
 var module_require_3_inited;
 
-describe("facade", function(){
+describe("facade", function() {
 
-    var POLL_INTERVAL = 50;
+  var POLL_INTERVAL = 50;
 
-    // describe("facade(mod)", function(){
-    //     facade('require');
-
-    //     it("could load a neuron module", function(done){
-    //         var timer = setInterval(function() {
-    //             if(module_require_loaded){
-    //                 expect( module_require_loaded === true );
-    //                 done();
-    //                 clearInterval(timer);
-    //             }
-    //         }, POLL_INTERVAL);
-    //     });
-        
-    //     it("will run `init` method if exists", function(done){
-    //         var timer = setInterval(function() {
-    //             if(module_require_inited){
-    //                 expect( module_require_inited === true );
-    //                 done();
-    //                 clearInterval(timer);
-    //             }
-    //         }, POLL_INTERVAL);
-    //     });
-    // });
-
-
-    describe("facade({mod}), to suppress interference, we use a new module", function(){
-        facade({
-            mod: 'require-2'
-        });
-
-        it("could load a neuron module", function(done){
-            var timer = setInterval(function() {
-                if(module_require_2_loaded){
-                    expect( module_require_2_loaded === true );
-                    done();
-                    clearInterval(timer);
-                }
-            }, POLL_INTERVAL);
-
-        });
-        
-        it("will run `init` method if exists", function(done){
-            var timer = setInterval(function() {
-                if(module_require_2_inited){
-                    expect( module_require_2_inited === true );
-                    done();
-                    clearInterval(timer);
-                }
-            }, POLL_INTERVAL);
-        });
-    
+  describe("facade(mod)", function() {
+    facade({
+      mod: 'require'
     });
 
-    describe("facade({mod, config})", function(){
-        it("could assign the value of the argument of `init` by `config`", function(done){
-            var atom = {};
-
-            facade({
-                mod: 'require-3',
-                data: {
-                    value: atom
-                }
-            });
-
-            var timer = setInterval(function() {
-                if(module_require_2_inited){
-                    expect( module_require_2_inited === true );
-                    done();
-                    clearInterval(timer);
-                }
-
-            }, POLL_INTERVAL);
-        });
-
-        // test #72:
-        it("should apply ranges when facading a package", function(done){
-            facade({
-                mod: 'range',
-                data: function (n) {
-                    expect(n).to.equal(1);
-                    done();
-                }
-            });
-        });
+    it("could load a neuron module", function(done) {
+      var timer = setInterval(function() {
+        if (module_require_loaded) {
+          expect(module_require_loaded === true);
+          done();
+          clearInterval(timer);
+        }
+      }, POLL_INTERVAL);
     });
+
+    it("will run `init` method if exists", function(done) {
+      var timer = setInterval(function() {
+        if (module_require_inited) {
+          expect(module_require_inited === true);
+          done();
+          clearInterval(timer);
+        }
+      }, POLL_INTERVAL);
+    });
+  });
+
+
+  describe("facade({mod}), to suppress interference, we use a new module", function() {
+    facade({
+      mod: 'require-2'
+    });
+
+    it("could load a neuron module", function(done) {
+      var timer = setInterval(function() {
+        if (module_require_2_loaded) {
+          expect(module_require_2_loaded === true);
+          done();
+          clearInterval(timer);
+        }
+      }, POLL_INTERVAL);
+
+    });
+
+    it("will run `init` method if exists", function(done) {
+      var timer = setInterval(function() {
+        if (module_require_2_inited) {
+          expect(module_require_2_inited === true);
+          done();
+          clearInterval(timer);
+        }
+      }, POLL_INTERVAL);
+    });
+
+  });
+
+  describe("facade({mod, config})", function() {
+    it("could assign the value of the argument of `init` by `config`", function(done) {
+      var atom = {};
+
+      facade({
+        mod: 'require-3',
+        data: {
+          value: atom
+        }
+      });
+
+      var timer = setInterval(function() {
+        if (module_require_2_inited) {
+          expect(module_require_2_inited === true);
+          done();
+          clearInterval(timer);
+        }
+
+      }, POLL_INTERVAL);
+    });
+
+    // test #72:
+    it("should apply ranges when facading a package", function(done) {
+      facade({
+        mod: 'range',
+        data: function(n) {
+          expect(n).to.equal(1);
+          done();
+        }
+      });
+    });
+  });
 
 });
-
-
