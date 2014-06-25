f5712a65be5f7f8f276bb796eed285a8acf716b1
kaelzhang <i@kael.me>
2014-06-25 20:23:06 +0800

    adds neuron.write method; updates version for node.js

--- index.js
+++ index.js
@@ -1,14 +1,22 @@
 'use strict';
 
 var fs = require('fs');
+var fse = require('fs-extra');
 var node_path = require('path');
-var dist = node_path.join(__dirname, 'dist', 'neuron.js');
+var file = node_path.join(__dirname, 'dist', 'neuron.js');
+var version = require('./package.json').cortex.version;
 
 exports.content = function (callback) {
-  fs.readFile(dist, callback);
+  fs.readFile(file, callback);
 };
 
 
 exports.version = function () {
-  return require('./cortex.json').version;
+  return version;
+};
+
+
+exports.write = function (dist, callback) {
+  dist = node_path.join(dist, 'neuron', version, 'neuron.js');
+  fse.copy(file, dist, callback);
 };
