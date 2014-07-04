0da54a9575ca5a79e4814d2a72545ceabec086fe
kaelzhang <i@kael.me>
2014-07-04 16:33:55 +0800

    module-manager: fixes a bug that leads to the wrong result of require.resolve; fixes #119

--- test/spec/define.js
+++ test/spec/define.js
@@ -1,73 +1,69 @@
+describe("define, feated with _use", function() {
+  describe("1. `exports.xxx = xxx`", function() {
+    it("module exports could be mixed into argument `exports`", function(done) {
 
-describe("define, feated with _use", function(){
-        
-    describe("1. `exports.xxx = xxx`", function(){
-        it("module exports could be mixed into argument `exports`", function(done){
+      _use('exports', function(e) {
+        expect(e.a === 1);
+        done();
+      });
+    });
+  });
 
-            _use('exports', function(e) {
-                expect( e.a === 1 );
-                done();
-            });
-        });
+  describe("2. `module.exports`", function() {
+    it("define module exports with module.exports", function(done) {
+
+      _use('module-exports', function(e) {
+        expect(e.a === 1);
+        done();
+      });
     });
-        
-    describe("2. `module.exports`", function(){
-        it("define module exports with module.exports", function(done){
-        
-            _use('module-exports', function(e) {
-                expect( e.a === 1 );
-                done();
-            });
-        });
+  });
+
+  describe("exports priority: 2 > 1", function() {
+    it("module.exports has higher priority", function(done) {
+
+      _use('exports-priority', function(e) {
+        expect(e.a === 1);
+        done();
+      });
+
     });
-        
-    describe("exports priority: 2 > 1", function(){
-        it("module.exports has higher priority", function(done){
-            
-            _use('exports-priority', function(e) {
-                expect( e.a === 1 );
-                done();
-            });
-        
-        });
-    });
+  });
 });
 
-
-describe("require", function(){
-  it("should throw error if module not found", function(done){
-    _use('mod-not-found', function (e) {
+describe("require", function() {
+  it("should throw error if module not found", function(done) {
+    _use('mod-not-found', function(e) {
       expect(/Cannot find module/i.test(e.message)).to.equal(true);
       done();
     });
   });
 
-  it("should throw error if require an id with `@`", function(done){
-    _use('require-at', function (e) {
+  it("should throw error if require an id with `@`", function(done) {
+    _use('require-at', function(e) {
       expect(/prohibited/.test(e.message)).to.equal(true);
       done();
     });
   });
 });
 
-
-describe("require.resolve()", function(){
-  define('require-resolve@*/lib/main.js', [], function(require, exports, module){
+describe("require.resolve()", function() {
+  define('require-resolve@*/lib/main.js', [], function(require, exports, module) {
     exports.a = require.resolve('./a.png');
   }, {
     main: true,
     map: {}
   });
 
-  it("could return the resolved filename", function(done){
-    _use('require-resolve', function (r) {
+  it("could return the resolved filename", function(done) {
+    _use('require-resolve', function(r) {
       expect(r.a).to.equal('mod/require-resolve/*/lib/a.png');
       done();
     });
   });
 
-  define('require-resolve2@*/lib/main.js', [], function(require, exports, module){
-    exports.resolve = function (n) {
+  define('require-resolve2@*/lib/main.js', [], function(require, exports, module) {
+    exports.resolve = function(n) {
       return require.resolve(n);
     }
   }, {
@@ -75,20 +71,10 @@ describe("require.resolve()", function(){
     map: {}
   });
 
-  it("will throw if out of range", function(done){
-    _use('require-resolve2', function (r) {
-      try {
-        r.resolve('../a.png');
-      } catch(e) {
-        expect(true).to.equal(false);
-      }
-
-      try {
-        r.resolve('../../a.png');
-      } catch(e) {
-        return;
-      }
-      expect(true).to.equal(false);
+  it("will throw if out of range", function(done) {
+    _use('require-resolve2', function(r) {
+      expect(r.resolve('../a.png')).to.equal('mod/require-resolve2/*/a.png');
+      expect(r.resolve('../../a.png')).to.equal(undefined);
       done();
     });
   });
