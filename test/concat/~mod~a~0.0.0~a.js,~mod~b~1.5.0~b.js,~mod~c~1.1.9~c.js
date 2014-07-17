9646660f5ecef32462a4ec06d6edaf2a6e9bea5e
kaelzhang <i@kael.me>
2014-07-18 00:52:10 +0800

    test: improve test cases for combo files; #123

--- test/concat/~mod~a~0.0.0~a.js,~mod~b~1.5.0~b.js,~mod~c~1.1.9~c.js
+++ test/concat/~mod~a~0.0.0~a.js,~mod~b~1.5.0~b.js,~mod~c~1.1.9~c.js
@@ -1,12 +1,12 @@
 'use strict';
 
-define('a@0.0.0', ['b@1.0.0'], function (require, exports, module) {
+define('a@0.0.0', ['b@^1.0.0'], function (require, exports, module) {
     var b = require('b');
     module.exports = b + 1;
 });
 
 
-define('b@1.0.0', ['c@~1.1.0'], function (require, exports, module) {
+define('b@1.5.0', ['c@~1.1.0'], function (require, exports, module) {
     var c = require('c');
     module.exports = c + 1;
 });
