c0ca78498e041021cc36eea1006f35d601c685e1
kaelzhang <i@kael.me>
2014-07-17 13:48:25 +0800

    build: complete build process; #123

--- node/build.js
+++ node/build.js
@@ -9,18 +9,25 @@ function get_version () {
   return require(package_json).cortex.version;
 }
 
-concat.full(function (err, content) {
-  if (err) {
-    return console.error(err.stack || err);
-  }
 
-  var path = node_path.join(__dirname, '..', 'dist', 'neuron.js');
-  content = content.replace('@VERSION', get_version());
-  fs.writeFile(path, content, function (err) {
+function write (type, filename) {
+  concat[type](function (err, content) {
     if (err) {
       return console.error(err.stack || err);
     }
 
-    console.log('write to "' + path + '"');
+    var path = node_path.join(__dirname, '..', 'dist', filename);
+    content = content.replace('@VERSION', get_version());
+    fs.writeFile(path, content, function (err) {
+      if (err) {
+        return console.error(err.stack || err);
+      }
+
+      console.log('write to "' + path + '"');
+    });
   });
-});
+}
+
+
+write('full', 'neuron.js');
+write('core', 'neuron-core.js');
