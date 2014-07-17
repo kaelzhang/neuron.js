746c4949ea92562d41922bb392e0dbbfc3401602
kaelzhang <i@kael.me>
2014-07-17 22:03:56 +0800

    fixes an error if mod is defined before used; #123

--- test/spec/facade.js
+++ test/spec/facade.js
@@ -174,3 +174,17 @@ describe("facade", function() {
     });
   });
 });
+
+
+var module_require_3_load_count = 0;
+
+describe("_use()", function() {
+  it("could prevent duplicate loading", function(done) {
+    _use('require-3', function() {
+      _use('require-3', function() {
+        expect(module_require_3_load_count === 1);
+        done();
+      })
+    })
+  });
+});
