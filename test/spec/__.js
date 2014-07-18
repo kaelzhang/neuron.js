8eaea642d2df38ed4e7d50c7272d03850f639f45
kaelzhang <i@kael.me>
2014-07-18 17:44:41 +0800

    redesign module definition; fixes references of main entry; #125

--- test/spec/__.js
+++ test/spec/__.js
@@ -1,14 +1,13 @@
-define('filename@*/lib/index.js', [], function (require, exports, module, __filename, __dirname) {
-  exports.__filename = __filename;
-  exports.__dirname = __dirname;
-}, {
-  main: true,
-  map: {}
-});
-
-
 describe("__filename and __dirname", function(){
   it("could access filename as __filename", function(done){
+    define('filename@*/lib/index.js', [], function (require, exports, module, __filename, __dirname) {
+      exports.__filename = __filename;
+      exports.__dirname = __dirname;
+    }, {
+      main: true,
+      map: {}
+    });
+
     _use('filename', function (f) {
       expect(f.__filename).to.equal('mod/filename/*/lib/index.js');
       expect(f.__dirname).to.equal('mod/filename/*/lib');
