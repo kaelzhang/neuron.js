f170414b54c5ad7e14689fa5e7580ac2cc4a466c
kaelzhang <i@kael.me>
2014-07-29 13:00:09 +0800

    index: adds neuron.dist(); #133

--- index.js
+++ index.js
@@ -1,27 +1,30 @@
 'use strict';
 
+var neuron = exports;
+
 var fs = require('fs');
 var fse = require('fs-extra');
 var node_path = require('path');
+var readJson = require('read-cortex-json');
 var file = node_path.join(__dirname, 'dist', 'neuron.js');
 var concat = require('./node/concat');
 
-exports.content = function (callback) {
+neuron.content = function (callback) {
   fs.readFile(file, callback);
 };
 
 
 // Methods to get the neuron core
-exports.core = concat.core;
+neuron.core = concat.core;
 
 
 var version = require('./package.json').cortex.version;
-exports.version = function () {
+neuron.version = function () {
   return version;
 };
 
 
-exports.write = function (dist, callback, force) {
+neuron.write = function (dist, callback, force) {
   fs.exists(dist, function (exists) {
     if (!exists) {
       return fse.copy(file, dist, callback);
@@ -39,4 +42,24 @@ exports.write = function (dist, callback, force) {
       fse.copy(file, dist, callback);
     });
   });
+};
+
+
+neuron.dist = function (dist, callback, force) {
+  var to = node_path.join(dist, version, 'neuron.js');
+  async.parallel([
+    function (done) {
+      neuron.write(dist, done, force);
+    },
+
+    function (done) {
+      readJson.enhanced(__dirname, function (err, pkg) {
+        if (err) {
+          return done(err);
+        }
+
+        readJson.save(dist, pkg, done);
+      });
+    }
+  ], callback);
 };
