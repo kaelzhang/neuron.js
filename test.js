171415a7db687b73636258c3b21d0db3455f5389
Kael <i@kael.me>
2015-09-27 16:01:36 +0800

    test travis-ci

--- test.js
+++ test.js
@@ -8,6 +8,7 @@ var spawns = require('spawns');
 var app = express();
 app.use(express.static(__dirname));
 app.listen(8030, function () {
+  console.log('Test server started at 8030');
   spawns('sh test.sh', {
     stdio: 'inherit'
 
