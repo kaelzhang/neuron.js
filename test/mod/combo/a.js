3413f8ccb2f115c21c14da7c9f8b9d4faff1c4bf
Kael <i@kael.me>
2015-09-28 21:28:34 +0800

    #150: format test fixtures

--- test/mod/combo/a.js
+++ test/mod/combo/a.js
@@ -1,15 +1,18 @@
-define('combo-a@0.0.0', ['./a'], function (require, exports, module) {
-    var a = require('./a');
+define('combo-a@0.0.0', ['./a'], function(require, exports, module) {
+  var a = require('./a');
 
-    exports.init = function (n) {
-        return a.a(n);
-    }
+  exports.init = function(n) {
+    return a.a(n);
+  }
 }, {
-    main: true
+  main: true,
+  map: {
+    ''
+  }
 });
 
-define('combo-a@0.0.0/a', [], function (require, exports, module) {
-    exports.a = function(n){
-        return n;
-    }
+define('combo-a@0.0.0/a', [], function(require, exports, module) {
+  exports.a = function(n) {
+    return n;
+  }
 });
